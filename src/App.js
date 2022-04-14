5d278c34842a0da24ae3398f92234e45e6dc1e56
NateRobinson <840501291@qq.com>
2022-04-14 15:46:40 +0800

    add did comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { DIDCommentWithSession } from "did-comment-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ marginBottom: 0 }}>
-          React App running in ABT Node
-        </p>
+        <p style={{ marginBottom: 0 }}>React App running in ABT Node</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +15,10 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
+        <DIDCommentWithSession objId="test-id" color="green" />
       </header>
     </div>
   );
